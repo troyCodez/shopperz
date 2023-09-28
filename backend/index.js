9d1ba2d45a1f6e95cfb51654a420c97c3b5f3d43
troydelageto <leroyroy033@gmail.com>
2023-09-28 12:09:32 +0100

    setup starter files for project

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const bcrypt = require('bcryptjs');
+
+const axios = require('axios');
+
+const cloudinary = require('cloudinary').v2
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello here we start</h1>
+    </div>
+  )
+}
+
+
+
+
+export default App;
