fcbc9568c1d8c43b136d1958391596ab78f28d7f
troydelageto <leroyroy033@gmail.com>
2023-09-28 12:53:31 +0100

    testing system by adding nodemailer paackage

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 
 const cloudinary = require('cloudinary').v2
 
+const nodemailer = require("nodemailer");
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test')
